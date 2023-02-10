05c58fb997f6d90e3c031af527212f312760166d
Luky Mulana <lukymulana@gmail.com>
2023-02-10 10:47:09 +0700

    4. Blade Templating Engine

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,67 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "name" => "Luky Mulana",
         "email" => "vilodrive28@gmail.com",
-        "image" => "Avatar.jpg"
+        "image" => "Avatar.jpg",
+        "title" => "About"
     ]);
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Luky Mulana",
+            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. In iusto debitis laborum officia necessitatibus impedit. Nobis quam eligendi voluptate dignissimos dolore, quod ratione, tenetur, incidunt fugit reiciendis consectetur aliquid. Iusto impedit consectetur illum sequi. Ducimus, unde eum. Omnis totam cumque optio animi deleniti! Doloremque architecto rem exercitationem deleniti consequuntur similique vero eaque. Qui fugiat nostrum vero hic, aperiam repellendus, facilis veniam repudiandae officiis neque exercitationem ex dolore aut quam iure enim? Quaerat animi odit qui? Eum blanditiis quo earum odio."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Avilo",
+            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. In iusto debitis laborum officia necessitatibus impedit. Nobis quam eligendi voluptate dignissimos dolore, quod ratione, tenetur, incidunt fugit reiciendis consectetur aliquid. Iusto impedit consectetur illum sequi. Ducimus, unde eum. Omnis totam cumque optio animi deleniti! Doloremque architecto rem exercitationem deleniti consequuntur similique vero eaque. Qui fugiat nostrum vero hic, aperiam repellendus, facilis veniam repudiandae officiis neque exercitationem ex dolore aut quam iure enim? Quaerat animi odit qui? Eum blanditiis quo earum odio."
+        ],
+    ];
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+// halaman single post
+Route::get('posts/{slug}', function($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Luky Mulana",
+            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. In iusto debitis laborum officia necessitatibus impedit. Nobis quam eligendi voluptate dignissimos dolore, quod ratione, tenetur, incidunt fugit reiciendis consectetur aliquid. Iusto impedit consectetur illum sequi. Ducimus, unde eum. Omnis totam cumque optio animi deleniti! Doloremque architecto rem exercitationem deleniti consequuntur similique vero eaque. Qui fugiat nostrum vero hic, aperiam repellendus, facilis veniam repudiandae officiis neque exercitationem ex dolore aut quam iure enim? Quaerat animi odit qui? Eum blanditiis quo earum odio."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Avilo",
+            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. In iusto debitis laborum officia necessitatibus impedit. Nobis quam eligendi voluptate dignissimos dolore, quod ratione, tenetur, incidunt fugit reiciendis consectetur aliquid. Iusto impedit consectetur illum sequi. Ducimus, unde eum. Omnis totam cumque optio animi deleniti! Doloremque architecto rem exercitationem deleniti consequuntur similique vero eaque. Qui fugiat nostrum vero hic, aperiam repellendus, facilis veniam repudiandae officiis neque exercitationem ex dolore aut quam iure enim? Quaerat animi odit qui? Eum blanditiis quo earum odio."
+        ],
+    ];
+
+    $new_post = [];
+
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
