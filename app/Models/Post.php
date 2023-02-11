178fa46d8d2b62a5393bd904fbb648c5b8caf354
Luky Mulana <lukymulana@gmail.com>
2023-02-11 17:54:32 +0700

    7. Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,31 +2,18 @@
 
 namespace App\Models;
 
-class Post
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Luky Mulana",
-            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. In iusto debitis laborum officia necessitatibus impedit. Nobis quam eligendi voluptate dignissimos dolore, quod ratione, tenetur, incidunt fugit reiciendis consectetur aliquid. Iusto impedit consectetur illum sequi. Ducimus, unde eum. Omnis totam cumque optio animi deleniti! Doloremque architecto rem exercitationem deleniti consequuntur similique vero eaque. Qui fugiat nostrum vero hic, aperiam repellendus, facilis veniam repudiandae officiis neque exercitationem ex dolore aut quam iure enim? Quaerat animi odit qui? Eum blanditiis quo earum odio."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Avilo",
-            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. In iusto debitis laborum officia necessitatibus impedit. Nobis quam eligendi voluptate dignissimos dolore, quod ratione, tenetur, incidunt fugit reiciendis consectetur aliquid. Iusto impedit consectetur illum sequi. Ducimus, unde eum. Omnis totam cumque optio animi deleniti! Doloremque architecto rem exercitationem deleniti consequuntur similique vero eaque. Qui fugiat nostrum vero hic, aperiam repellendus, facilis veniam repudiandae officiis neque exercitationem ex dolore aut quam iure enim? Quaerat animi odit qui? Eum blanditiis quo earum odio."
-        ],
-    ];
+    use HasFactory;
 
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
+    // protected $fillable = [
+    //     'title',
+    //     'excerpt',
+    //     'body'
+    // ];
 
-    public static function find($slug)
-    {
-        $posts = static::all();
-        return $posts->firstWhere('slug', $slug);
-    }
+    protected $guarded = ['id'];
 }
